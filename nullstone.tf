f93e64afe8cd445c4b76147d789a6c3531d0dcb5
Brad Sickles <brad.sickles@gmail.com>
2022-01-27 16:36:43 -0500

    Utilizing project_id from key_file from delegator

--- nullstone.tf
+++ nullstone.tf
@@ -23,12 +23,15 @@ provider "google" {}
 // We will need to be able to support secondary providers since the root domain
 //   is typically managed in a separate account from non-production environments
 provider "google" {
-  credentials = base64decode(data.ns_connection.domain.outputs.delegator["key_file"])
   alias       = "domain"
+  credentials = local.delegator_key_file
+  project     = local.delegator_project_id
 }
 
 locals {
-  domain_name        = data.ns_connection.domain.outputs.name
-  domain_zone_id     = data.ns_connection.domain.outputs.zone_id
-  domain_nameservers = data.ns_connection.domain.outputs.nameservers
+  domain_name          = data.ns_connection.domain.outputs.name
+  domain_zone_id       = data.ns_connection.domain.outputs.zone_id
+  domain_nameservers   = data.ns_connection.domain.outputs.nameservers
+  delegator_key_file   = base64decode(data.ns_connection.domain.outputs.delegator["key_file"])
+  delegator_project_id = lookup(jsondecode(local.delegator_key_file), "project_id")
 }
